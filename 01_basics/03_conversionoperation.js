4b62ec6549b7085b8b5fc79449fe192df9b736dd
KeshavAgarwal8 <162178547+KeshavAgarwal8@users.noreply.github.com>
2025-09-13 03:32:55 +0000

    dataypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score = "hitesh"
+
+// console.log(typeof score);   -> return string
+//console.log(typeof(score));    -> second method
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);    -> it changes string to number
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
