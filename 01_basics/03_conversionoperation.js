7d4b2812a5322b0e1ac34f18581eff931bfc0ae5
KeshavAgarwal8 <162178547+KeshavAgarwal8@users.noreply.github.com>
2025-09-13 03:55:34 +0000

    operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -27,3 +27,49 @@ let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
+  
+
+//                 operation   
+
+
+let value  = 3
+let negvalue = -value
+// console.log(negvalue);
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+
+// console.log("1" +3);
+// console.log(1 + "3");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+
+// console.log(+true)     ->1
+// console.log(+"")   -> 0
+
+
+
+// let gameCounter =100;
+// gameCounter++;
+// console.log(gameCounter);   -> 101
+
+
+
+
+
+
