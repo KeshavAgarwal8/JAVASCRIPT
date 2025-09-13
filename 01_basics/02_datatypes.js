a32e5a14c4d04c48887c443336662eac6df11219
KeshavAgarwal8 <162178547+KeshavAgarwal8@users.noreply.github.com>
2025-09-13 01:47:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version
+
+
+// alert(3+3) gives error because we are not using browser we are using nodejs
+
+
+// code should be in a proper format means readable
+
+let name = "Keshav"
+let age = 18
+let isLoggedin = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof age)
+
+console.log(typeof null)   // gives output as object
+
+console.log(typeof undefined)  // undefined
+
+
+
